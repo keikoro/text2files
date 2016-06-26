9de762b491a77a3148034787cee6246046081fc1
K Kollmann <git@k.kollmann.moe>
2016-06-26 13:35:37 +0200

    add bash script for turning lines of text into file names

---
+++ text2files.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Create file names from lines of text
+# Copyright (c) 2016 K Kollmann <code∆k.kollmann·moe>
+
+text="You need to provide the name of a file
+        whose text contents
+        \nshould get
+        turned into (empty) file names line by line.
+        \nUsage: sh text2files.sh yourfile"
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo $text
+    exit
+else
+   fname=$1
+fi
+
+echo "Reading $1..."
+while read line
+do
+    if [ "$line" != "" ]; then
+        if [ -f $line ]; then
+            echo "FILE ALREADY EXISTS: $line – skipping"
+        else
+            echo "creating file $line"
+           touch "$line"
+        fi
+    fi
+done < $fname
+echo "Done."
